84c2566cea55b0e5c85f01ed71e8162b46088155
Darby Smith <newman.darby2@gmail.com>
2022-08-24 11:46:57 -0400

    Pass test for iterations 1-2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,29 @@
 class Venue
+  attr_reader :name, 
+              :capacity, 
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end 
+
+  def add_patron(patron)
+    @patrons << patron
+  end 
+
+  def yell_at_patrons
+    @patrons.each do |patron| 
+      patron.upcase!
+    end
+  end 
+
+  def over_capacity?
+    if @patrons.length > @capacity
+      true 
+    else 
+      false
+    end 
+  end 
 end
